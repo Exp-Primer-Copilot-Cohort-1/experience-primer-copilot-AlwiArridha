2bf48d366c068791ffadd51aa92a855e3f89d73f
Kajeee <146497795+AlwiArridha@users.noreply.github.com>
2024-06-05 04:23:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember(){
+    console.log("I am a member of this team");
+    console.log("I can do the following things: ");
+    console.log("I can code");
+    console.log("I can design");
+    console.log("I can test");
+    console.log("I can deploy");
+}
